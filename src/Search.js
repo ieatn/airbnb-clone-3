67f1011c06aff257ba1aa18b1b41f9e6aa51ad6f
Dennis Zhang <ieatnubcake@gmail.com>
2022-01-21 12:24:46 -0800

    commit

--- src/Search.js
+++ src/Search.js
@@ -5,8 +5,10 @@ import 'react-date-range/dist/theme/default.css';
 import {DateRangePicker} from 'react-date-range'; 
 import PeopleIcon from '@mui/icons-material/People';
 import {Button} from '@material-ui/core';
+import {useNavigate} from 'react-router-dom' 
 
 function Search() {
+  const navigate = useNavigate();
   const [startDate, setStartDate] = useState(new Date());
   const [endDate, setEndDate] = useState(new Date());
   const selectionRange = {
@@ -24,7 +26,7 @@ function Search() {
     <DateRangePicker ranges={[selectionRange]} onChange={handleSelect} />
     <h2>Number of guests <PeopleIcon /></h2>
     <input type="number" min={0} defaultValue={2} />
-    <Button>Search Airbnb</Button>
+    <Button onClick={() => navigate('/search')}>Search Airbnb</Button>
   </div>;
 }
 
